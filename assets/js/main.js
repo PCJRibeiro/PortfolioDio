8bf44f4c47cfeb975d3ffbe92ef500a3587e5cf6
PCJRibeiro <pcjr2003@gmail.com>
2024-07-20 23:01:29 -0300

    adicionando languages

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,6 +34,11 @@ function updateHardSkills(profileData){
     hardSkills.innerHTML = profileData.skills.hardSkills.map(skill=> `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
 }
 
+function updateLanguages(profileData){
+    const languages =document.getElementById('profile.languages')
+    languages.innerHTML = profileData.languages.map(language=> `<li>${language}</li>`).join('')
+}
+
 (async () => {
 
     const profileData = await fetchProfileData()
@@ -41,4 +46,5 @@ function updateHardSkills(profileData){
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
     updateHardSkills(profileData)
+    updateLanguages(profileData)
 })()
